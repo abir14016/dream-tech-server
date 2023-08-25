9819d59004ee89e02fa2600320596367a4c6ddc8
abir14016 <abir14016@gmail.com>
2023-08-25 17:16:01 +0600

    get all products from database

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -8,9 +9,42 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+//connect with mongodb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.boat8.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db("dream-tech").collection("products");
+
+        //all products get API
+        app.get("/product", async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+//root rout
 app.get('/', (req, res) => {
     res.send('Hello From dream tech!')
 })
+//root rout
 
 app.listen(port, () => {
     console.log(`Dream tech server listening on port ${port}`)
